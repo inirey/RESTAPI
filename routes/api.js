97f61025dca2288a812b39079157e2764e2a7fc2
KUVIANTIINDONESIANET <82393188+KUVIANTIINDONESIANET@users.noreply.github.com>
2021-07-03 12:41:46 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["KUV-API", "MarioApi"];
+const listkey = ["Mario-Api", "KUV-API"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
