d2c677148fcb58e9dd298009a6a2764494e2dfbe
Ardy <86863270+ArdyBotzz@users.noreply.github.com>
2021-07-04 14:33:51 +0800

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "Mario_us"
+var creator = "ArdyStore"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["Mario-Api", "KUV-API"];
+const listkey = ["Ardy-Apikey", "MaxTeam"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
