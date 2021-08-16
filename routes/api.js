be25c357c8d6bc3116eaf34a89a35da18665663e
Rey <85196372+inirey@users.noreply.github.com>
2021-08-16 17:21:38 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["Ardy-Apikey", "MaxTeam"];
+const listkey = ["apirey", "ditofficial"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
