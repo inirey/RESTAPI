9b091b442ec91cbf8ccf655d27fb7b51c9a79212
Sekha <gansrey97@gmail.com>
2022-01-29 20:18:25 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sekha.tech';
+var url = 'https://api.reysekha.xyz';
 try {
-if(url == 'https://api.sekha.tech') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
