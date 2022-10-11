6c8eb4e481cf58b9abb4eb68227b69f72f7d0796
Reysekha <gansrey97@gmail.com>
2022-10-11 07:43:14 +0700

    New

--- database/db.js
+++ database/db.js
@@ -3,16 +3,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.reysekha.xyz';
+var url = 'https://api.sekha.me';
 try {
-if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, ZhirrrGanss','green'))
+  console.log(color('Connected correctly to server, oke','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
